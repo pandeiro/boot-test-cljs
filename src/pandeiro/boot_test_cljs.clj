be17d804a5082ca6e9ffdd453a9271e6967902f8
Murphy McMahon <pandeiro@gmail.com>
2015-02-10 00:13:59 -0200

    refactor (working)

--- src/pandeiro/boot_test_cljs.clj
+++ src/pandeiro/boot_test_cljs.clj
@@ -21,7 +21,7 @@
   ([browser proxyserver port]
    (WebClient. browser proxyserver port)))
 
-(defn test-cljs-runner-ns-src [namespaces]
+(defn runner-src [namespaces]
   (let [required-ns (s/join " " namespaces)
         quoted-ns (s/join " " (map #(str "'" %) namespaces))]
     (format "
@@ -47,7 +47,7 @@
      required-ns
      quoted-ns)))
 
-(defn test-cljs-html-page [basename]
+(defn html-page-src [basename]
   (format
    (str "<!doctype html><html><meta charset=\"utf-8\">"
         "<body><script src=\"%s.js\"></script></body></html>")
@@ -73,58 +73,65 @@
 (defn silence-htmlunit! []
   (.setLevel (Logger/getLogger "com.gargoylesoftware.htmlunit") Level/OFF))
 
-(require 'clojure.set)
-
-(deftask test-cljs
-  "Test one or more ClojureScript namespaces by compiling them and checking that
-  their tests run without producing any exceptions."
-  [n namespaces NAMESPACE #{sym} "Namespaces whose tests will be run."]
-  (let [rsc-dir   (core/temp-dir!)
-        src-dir   (core/temp-dir!)
-        basename  (atom "")
-        req-ns    (conj namespaces 'pandeiro.test-cljs.runner)
-        http-port (free-port)]
-    (comp
-     (core/with-pre-wrap fileset
-       (file/empty-dir! rsc-dir src-dir)
-       (reset! basename (str "boot_test_cljs_" (random-str)))
-       (let [cljs-main  (io/file rsc-dir (str @basename ".cljs.edn"))
-             html-page  (io/file rsc-dir (str @basename ".html"))
-             test-cljs-ns-dir (doto (io/file src-dir "pandeiro/boot_test_cljs")
-                                (.mkdirs))
-             cljs-test-runner (io/file test-cljs-ns-dir "runner.cljs")]
-         (spit cljs-test-runner (test-cljs-runner-ns-src namespaces))
-         (spit cljs-main (pr-str {:require  namespaces
+(deftask ^:private scaffold
+  "Creates the test runner, compiler config and HTML files needed for testing"
+  [b basename   BASE str    "The basename of the files used for tests"
+   n namespaces NS   #{sym} "Namespaces whose tests will be run."]
+  (let [resource-dir (core/temp-dir!)
+        source-dir   (core/temp-dir!)]
+    (core/with-pre-wrap fileset
+      (file/empty-dir! resource-dir source-dir)
+      (let [ns-dir    (doto (io/file source-dir "pandeiro/boot_test_cljs")
+                        (.mkdirs))
+            runner    (io/file ns-dir "runner.cljs")
+            cljs-cfg  (io/file resource-dir (str basename ".cljs.edn"))
+            html-page (io/file resource-dir (str basename ".html"))]
+         (spit runner (runner-src namespaces))
+         (spit cljs-cfg (pr-str {:require  namespaces
                                   :init-fns ['pandeiro.boot-test-cljs.runner/run]}))
-         (spit html-page (test-cljs-html-page @basename))
+         (spit html-page (html-page-src basename))
          (-> (->> fileset
                core/input-files
                (core/by-ext [".cljs.edn"])
                (core/rm fileset))
-           (core/add-source src-dir)
-           (core/add-resource rsc-dir)
-           (core/commit!))))
-     (do
-       ;;(swap! util/*verbosity* + (* -1 @util/*verbosity*)) ; turn off cljs output
-       (cljs :optimizations :none, :source-map true))
-     (do
-       ;;(swap! util/*verbosity* + (* -1 @util/*verbosity*)) ; turn output back on
-       (serve :dir "target" :port http-port))
-     (core/with-pre-wrap fileset
-       (silence-htmlunit!)
-       (let [test-page-url (format "http://localhost:%d/%s.html" http-port @basename)
-             wc            (web-client)]
-         (util/info "<< HtmlUnit connecting to %s... >>\n" test-page-url)
-         (try
-           (.getPage wc test-page-url)
-           (catch Exception e
-             (let [{:keys [message summary inner]} (extract-test-summary e)]
-               (util/info (str message "\n\n"))
-               ;;(println inner)
-               (when (> (apply + (map summary [:fail :error])) 0)
-                 (throw (ex-info "Some tests failed or errored" summary)))))
-           (finally
-             (util/info "<< Closing all HtmlUnit webclients... >>\n")
-             (.closeAllWindows wc))))
-       fileset))))
+           (core/add-source source-dir)
+           (core/add-resource resource-dir)
+           (core/commit!))))))
+
+(deftask ^:private execute
+  "Runs the tests and captures the results"
+  [b basename  BASE str "The basename of the files used for tests"
+   p port      PORT int "The port the test server is running on"]
+  (core/with-pre-wrap fileset
+    (silence-htmlunit!)
+    (let [url (format "http://localhost:%d/%s.html" port basename)
+          wc  (web-client)]
+      (util/info "<< HtmlUnit connecting to %s... >>\n" url)
+      (try
+        (.getPage wc url)
+        (catch Exception e
+          (let [{:keys [message summary inner]} (extract-test-summary e)]
+            (util/info (str message "\n\n"))
+            (when (> (apply + (map summary [:fail :error])) 0)
+              (throw (ex-info "Some tests failed or errored" summary)))))
+        (finally
+          (util/info "<< Closing all HtmlUnit webclients... >>\n")
+          (.closeAllWindows wc))))
+    fileset))
+
+(deftask test-cljs
+  "Test one or more ClojureScript namespaces by compiling them and checking that
+  their tests run without producing any exceptions."
+  [n namespaces NS #{sym} "Namespaces whose tests will be run."]
+  (let [basename  (str "boot_test_cljs_" (random-str))
+        http-port (free-port)]
+    (comp
+     (scaffold :basename      basename
+               :namespaces    namespaces)
+     (cljs     :optimizations :none
+               :source-map    true)
+     (serve    :port          http-port
+               :silent        true)
+     (execute  :basename      basename
+               :port          http-port))))
 
